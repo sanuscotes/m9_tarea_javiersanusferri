fc4463e025e1845df8a77c062dedaf4f5797e4c4
Javier Sanus <jsanus@javier-sanus.jover.local>
2026-03-11 14:04:35 +0100

    fix dash run method

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ def display_page(pathname):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))
-    app.run_server(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
